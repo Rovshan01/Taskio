0ab8c2f215da4f29ef0062bf8118c3735c40f4fe
Rovshan Khasammadov <rovshankhasammadov@gmail.com>
2025-12-18 17:44:49 +0400

    initial project setup

---
+++ lib/core/constants/app_texts.dart
@@ -0,0 +1,8 @@
+class AppTexts {
+  static const onboarding1Title = "Welcome to taskio";
+  static const onboarding1Subtitle = "Whats going to happen tomorrow?";
+  static const onboarding2Title = "Work happens";
+  static const onboarding2Subtitle = "Get notified when work happens.";
+  static const onboarding3Title = "Tasks and assign";
+  static const onboarding3Subtitle = "Task and assign them to colleagues.";
+}
